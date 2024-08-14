1d76617b71bdcd85b9bb211bbcc1a9a028154b34
keenroudy22 <kroudebush96@gmail.com>
2024-08-14 14:20:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-def calculate_macros(gender, age, height, weight, goal_weight, activity_level):
+def calculate_macros(gender, age, height_cm, weight_kg, goal_weight_kg, activity_level):
     if gender == "male":
-        bmr = 10 * weight + 6.25 * height - 5 * age + 5
+        bmr = 10 * weight_kg + 6.25 * height_cm - 5 * age + 5
     else:
-        bmr = 10 * weight + 6.25 * height - 5 * age - 161
+        bmr = 10 * weight_kg + 6.25 * height_cm - 5 * age - 161
 
     tdee = bmr * float(activity_level)
 
@@ -14,7 +14,7 @@ def calculate_macros(gender, age, height, weight, goal_weight, activity_level):
     goal_calories = tdee - 500
 
     # Protein: 2g/kg of current body weight
-    protein = 2 * weight
+    protein = 2 * weight_kg
 
     # Fats: 25% of total calories, 1g = 9 calories
     fats = (goal_calories * 0.25) / 9
@@ -38,12 +38,18 @@ def calculate():
     data = request.json
     gender = data['gender']
     age = int(data['age'])
-    height = float(data['height'])
-    weight = float(data['weight'])
-    goal_weight = float(data['goalWeight'])
+    height_feet = int(data['heightFeet'])
+    height_inches = int(data['heightInches'])
+    weight_lbs = float(data['weight'])
+    goal_weight_lbs = float(data['goalWeight'])
     activity_level = data['activityLevel']
 
-    macros = calculate_macros(gender, age, height, weight, goal_weight, activity_level)
+    # Convert height to cm and weight to kg
+    height_cm = (height_feet * 30.48) + (height_inches * 2.54)
+    weight_kg = weight_lbs / 2.205
+    goal_weight_kg = goal_weight_lbs / 2.205
+
+    macros = calculate_macros(gender, age, height_cm, weight_kg, goal_weight_kg, activity_level)
     return jsonify(macros)
 
 if __name__ == '__main__':
